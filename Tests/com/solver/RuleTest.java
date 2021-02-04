db476e552d22fc6d89281c74db79acbe72397751
bando <dummy@dummy.com>
2021-02-04 10:27:04 +0900

    空のRuleクラスとTestクラスを作成

---
+++ Tests/com/solver/RuleTest.java
@@ -0,0 +1,14 @@
+package com.solver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
