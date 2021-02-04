1aa07a9fab24e840080a5ccbfd3387ad9abac388
ssc-bando <satoshi_bando@solver.co.jp>
2021-02-04 13:12:40 +0900

    グーで負けるケースを追加

--- src/com/solver/Janken/Rule.java
+++ src/com/solver/Janken/Rule.java
@@ -1,7 +1,19 @@
 package com.solver.Janken;
 
 public class Rule {
-    public static Result Judge(Pattern self, Pattern opponent) {
-        return Result.Win;
+    public static Result JudgeByStone(Pattern opponent) {
+        switch(opponent) {
+            case Stone:
+                return Result.Draw;
+            case Scissor:
+                return Result.Win;
+            case Paper:
+            default:
+                return Result.Lose;
+        }
+    }
+
+    public static Result Judge(Pattern my, Pattern opponent) {
+        return JudgeByStone(opponent);
     }
 }
